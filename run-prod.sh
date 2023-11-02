064335fc3757eed74360b408d43f190e8ce5d25b
apple <apple15634145@gmail.com>
2023-11-02 19:08:33 +0800

    1

--- run-prod.sh
+++ run-prod.sh
@@ -3,14 +3,13 @@
 # Go 可执行文件名
 name="goframe-admin"
 # 日志文件名
-logFile="output_$(date '+%Y%m%d_%H%M%S').log"
-errorFile="error_$(date '+%Y%m%d_%H%M%S').log"  # 新增的错误日志文件名
+logFile="logs/nohub_$(date '+%Y%m%d_%H%M%S').log"
 configFile="config.prod.yaml"
 
 # 执行发布流程
 echo "Fetching latest code..."
 git fetch --all
-git reset --hard origin/master
+git reset --hard origin/main
 
 #设置操作权限
 chmod +x ./$name
@@ -22,7 +21,7 @@ sleep 2  # 等待程序停止
 
 # 启动 Go 程序并将输出重定向到日志文件
 echo "启动 Go 程序..."
-nohup ./$name --gf.gcfg.file=$configFile > $logFile 2> $errorFile &
+nohup ./$name --gf.gcfg.file=$configFile > $logFile 2>&1 &
 
 # 如果你想在脚本中输出错误信息，可以使用 cat 命令
 cat $errorFile
